f4e05e8478a2c648a032e70a91e13a0c55cb60df
Sarah Kirk <kirkse710@gmail.com>
2018-01-04 10:16:22 -0800

    Builds Scaffolding for App -Writes preliminary CRUD function for Burrito Controller -Moves and updates files in the file tree -Updates index action in Burritos Controller

--- app/Http/Controllers/BurritosController.php
+++ app/Http/Controllers/BurritosController.php
@@ -8,6 +8,39 @@ class BurritosController extends Controller
 {
     public function index()
     {
-      return view('burritos.index' , ['burritos'=>Burrito::all()]);
+      $burritos = Burrito::all();
+
+      return View::make('burritos.index')
+          ->with('burritos' , $burritos);
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update($id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+      
     }
 }
