82706082e0def5978586160e19a0ee78e0e3f33e
Sarah Kirk <kirkse710@gmail.com>
2018-01-03 17:22:20 -0800

    adds index function to burrito controller

--- app/Http/Controllers/BurritosController.php
+++ app/Http/Controllers/BurritosController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class BurritosController extends Controller
 {
-    //
+    public function index()
+    {
+      return view('burritos.index' , ['burritos'=>Burrito::all()]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('burritos', 'BurritosController');
+
+Route::get('/', 'BurritosController@index');
